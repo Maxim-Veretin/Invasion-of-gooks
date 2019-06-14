8399d4dc529eb9c861914c273238a37773c2958f
Vlazzers <alexander.screed@gmail.com>
2019-06-15 01:04:00 +0700

    YEY

--- Invasion_of_gooks/Invasion_of_gooks/MainWindow.xaml.cs
+++ Invasion_of_gooks/Invasion_of_gooks/MainWindow.xaml.cs
@@ -23,6 +23,10 @@ namespace Invasion_of_gooks
         System.Windows.Threading.DispatcherTimer Timer;
         DateTime date = new DateTime();
 
+        //координаты вертолёта 
+        public int x = 633;
+        public int y = 500;
+        public Polygon helicopter = new Polygon();
         public MainWindow()
         {
             InitializeComponent();
@@ -32,24 +36,24 @@ namespace Invasion_of_gooks
             Timer.Tick += new EventHandler(DispatcherTimer_Tick);
             Timer.Interval = new TimeSpan(0, 0, 1);
 
-            Point helicoptercenter = new Point(683, 500);
+
             //создание объекта вертолёта
-            Polygon helicopter = new Polygon
-            {
-                //установка цвета обводки, цвета заливки и толщины обводки
-                Stroke = Brushes.Black,
-                Fill = Brushes.Green,
-                StrokeThickness = 2,
-                //позиционирование объекта
-                HorizontalAlignment = HorizontalAlignment.Left,
-                VerticalAlignment = VerticalAlignment.Center
-            };
+
+
+            //установка цвета обводки, цвета заливки и толщины обводки
+            helicopter.Stroke = Brushes.Black;
+            helicopter.Fill = Brushes.Green;
+            helicopter.StrokeThickness = 2;
+            //позиционирование объекта
+            helicopter.HorizontalAlignment = HorizontalAlignment.Left;
+            helicopter.VerticalAlignment = VerticalAlignment.Center;
+            
             //создание точек многоугольника
-            Point Point1 = new Point(helicoptercenter.X, helicoptercenter.Y - 50);
-            Point Point2 = new Point(helicoptercenter.X + 50, helicoptercenter.Y);
-            Point Point3 = new Point(helicoptercenter.X + 50, helicoptercenter.Y + 50);
-            Point Point4 = new Point(helicoptercenter.X - 50, helicoptercenter.Y + 50);
-            Point Point5 = new Point(helicoptercenter.X - 50, helicoptercenter.Y);
+            Point Point1 = new Point(x, y - 50);
+            Point Point2 = new Point(x + 50, y);
+            Point Point3 = new Point(x + 50, y + 50);
+            Point Point4 = new Point(x - 50, y + 50);
+            Point Point5 = new Point(x - 50, y);
             //создание и заполнение коллекции точек
             PointCollection helicopterCollection = new PointCollection
             {
@@ -100,6 +104,93 @@ namespace Invasion_of_gooks
         {
             date = date.AddSeconds(1);
             stopwatch.Content = date.Hour.ToString() + " : " + date.Minute.ToString() + " : " + date.Second.ToString();
+            can.Children.Remove(helicopter);
+            //установка цвета обводки, цвета заливки и толщины обводки
+            helicopter.Stroke = Brushes.Black;
+            helicopter.Fill = Brushes.Green;
+            helicopter.StrokeThickness = 2;
+            //позиционирование объекта
+            helicopter.HorizontalAlignment = HorizontalAlignment.Left;
+            helicopter.VerticalAlignment = VerticalAlignment.Center;
+
+            //создание точек многоугольника
+            Point Point1 = new Point(x, y - 50);
+            Point Point2 = new Point(x + 50, y);
+            Point Point3 = new Point(x + 50, y + 50);
+            Point Point4 = new Point(x - 50, y + 50);
+            Point Point5 = new Point(x - 50, y);
+            //создание и заполнение коллекции точек
+            PointCollection helicopterCollection = new PointCollection
+            {
+                Point1,
+                Point2,
+                Point3,
+                Point4,
+                Point5
+            };
+            //установка коллекции точек в объект многоугольник
+            helicopter.Points = helicopterCollection;
+            //добавление многоугольника в сцену
+            can.Children.Add(helicopter);
+
+
+
+
+        }
+        private void Key_Down(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                //если нажата стрелочка вверх, то коордианта вертолётика смещается вверх 
+                case Key.Up:
+                    helicopter.RenderTransform = new TranslateTransform(0, -10);
+                    y -= 10;
+                    break;
+
+                //если нажата стрелочка вправо, то коордианта вертолётика смещается вправо 
+                case Key.Right:
+                    helicopter.RenderTransform = new TranslateTransform(+10, 0);
+                    x += 10;
+                    break;
+
+                //если нажата стрелочка вниз, то коордианта вертолётика смещается вниз 
+                case Key.Down:
+                    helicopter.RenderTransform = new TranslateTransform(0, +10);
+                    y += 10;
+                    break;
+
+                //если нажата стрелочка влево, то коордианта вертолётика смещается влево 
+                case Key.Left:
+                    helicopter.RenderTransform = new TranslateTransform(-10, 0);
+                    x -= 10;
+                    break;
+
+                //если нажата клавиша Escape, то вызывается меню паузы 
+                //case Key.Escape:
+                //    PauseMenu pause = new PauseMenu();
+                //    Timer.Stop();
+                //    if (pause.ShowDialog() == true)
+                //    {
+
+                //    }
+                //    else
+                //    {
+                //        Timer.Start();
+                //    }
+                //    break;
+
+                //если нажата клавиша Z, то применяется способность и вызывается её анимация 
+                case Key.Z:
+                    break;
+
+                //если нажата клавиша X, то применяется способность и вызывается её анимация 
+                case Key.X:
+                    break;
+
+                //если нажата клавиша C, то применяется способность и вызывается её анимация 
+                case Key.C:
+                    break;
+            }
         }
     }
-}
+}//доброе утро я только что проснулся
